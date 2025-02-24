f405554e78d204b6ce6b64d44a2b97296c3e23f6
ThanhTNV <thanhtran190304@gmail.com>
2025-02-24 15:55:07 +0700

    fix: separate the test into 4 test

--- src/test/java/TestCalculatorCsv.java
+++ src/test/java/TestCalculatorCsv.java
@@ -1,17 +1,38 @@
 import static org.junit.Assert.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class TestCalculatorCsv {
-    Calculator math = new Calculator();
+    Calculator math;
 
-    @ParameterizedTest
+    @BeforeEach
+    void init() {
+        math = new Calculator();
+    }
+
+    @ParameterizedTest(name = "Test Add Function")
     @CsvFileSource(resources = "/cal.csv", numLinesToSkip = 1)
-    void TestCalculator(int a, int b, int add, int minus, int multiply, float divide, float delta) {
+    void TestAdd(int a, int b, int add, int minus, int multiply, float divide) {
         assertEquals(math.add(a, b), add);
+    }
+
+    @ParameterizedTest(name = "Test Minus Function")
+    @CsvFileSource(resources = "/cal.csv", numLinesToSkip = 1)
+    void TestMinus(int a, int b, int add, int minus, int multiply, float divide) {
         assertEquals(math.minus(a, b), minus);
+    }
+
+    @ParameterizedTest(name = "Test Multiply Function")
+    @CsvFileSource(resources = "/cal.csv", numLinesToSkip = 1)
+    void TestMultiply(int a, int b, int add, int minus, int multiply, float divide) {
         assertEquals(math.multiply(a, b), multiply);
-        assertEquals(math.divide(a, b), divide, delta);
+    }
+
+    @ParameterizedTest(name = "Test Divide Function")
+    @CsvFileSource(resources = "/cal.csv", numLinesToSkip = 1)
+    void TestDivide(int a, int b, int add, int minus, int multiply, float divide) {
+        assertEquals(math.divide(a, b), divide, 0);
     }
 }
