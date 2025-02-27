fced175390aadd97e2fd9c7127287598f4063d02
ThanhTNV <thanhtran190304@gmail.com>
2025-02-27 15:16:26 +0700

    fix: remove before each

--- src/test/java/TestCalculatorCsv.java
+++ src/test/java/TestCalculatorCsv.java
@@ -1,32 +1,26 @@
 import static org.junit.Assert.assertEquals;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class TestCalculatorCsv {
-    Calculator math;
-
-    @BeforeEach
-    void init() {
-        math = new Calculator();
-    }
+    Calculator math = new Calculator();
 
     @ParameterizedTest(name = "Test Add Function")
     @CsvFileSource(resources = "/cal.csv", numLinesToSkip = 1)
-    void TestAdd(int a, int b, int add, int minus, int multiply, float divide) {
+    void TestAdd(int a, int b, int add) {
         assertEquals(math.add(a, b), add);
     }
 
     @ParameterizedTest(name = "Test Minus Function")
     @CsvFileSource(resources = "/cal.csv", numLinesToSkip = 1)
-    void TestMinus(int a, int b, int add, int minus, int multiply, float divide) {
+    void TestMinus(int a, int b, int add, int minus) {
         assertEquals(math.minus(a, b), minus);
     }
 
     @ParameterizedTest(name = "Test Multiply Function")
     @CsvFileSource(resources = "/cal.csv", numLinesToSkip = 1)
-    void TestMultiply(int a, int b, int add, int minus, int multiply, float divide) {
+    void TestMultiply(int a, int b, int add, int minus, int multiply) {
         assertEquals(math.multiply(a, b), multiply);
     }
 
